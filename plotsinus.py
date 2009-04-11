8dc92d0f1c435382d0b41e9fad4e45ae601351d1
Yves Frederix <yves.frederix@cs.kuleuven.be>
2009-04-11 20:55:34 +0200

    verander de lijnkleur

--- plotsinus.py
+++ plotsinus.py
@@ -6,6 +6,6 @@ import pylab as pl
 
 x = pl.linspace(0, 2*pl.pi, 151)
 pl.figure()
-pl.plot(x, pl.sin(x), linewidth=2, label='Sinus')
+pl.plot(x, pl.sin(x), 'r', linewidth=2, label='Sinus')
 pl.legend(loc='best')
 pl.show()
