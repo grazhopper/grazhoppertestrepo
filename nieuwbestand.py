18d38f538ad12c888d7f93c11e9b6e99ed307273
Grazhopper <grazhopper@gmail.com>
2009-04-11 23:06:15 +0200

    en nog maar een nieuwe regel

--- nieuwbestand.py
+++ nieuwbestand.py
@@ -4,4 +4,5 @@ print 'aanpassing van nieuw bestand'
 print '--> precies was de vorige brnach-v1 toch niet jusit... nu wel daarentegen!'
 print '--> nieuwe regel'
 print '--> en nog een regel'
+print '--> en nog maar ne regel'
 
